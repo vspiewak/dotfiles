f95783e2ce69a097da0adb7a72f9d5f2b3e4dd47
Vincent Spiewak <vspiewak@gmail.com>
2025-07-16 01:52:50 +0200

    Add dockutil script

---
+++ home/.chezmoiscripts/darwin/run_onchange_after_configure-dock.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -eufo pipefail
+
+trap 'killall Dock' EXIT
+
+# removed
+declare -a remove_labels=(
+	Launchpad
+	Messages
+	Mail
+	Maps
+	Photos
+	FaceTime
+	Calendar
+	Contacts
+	Reminders
+	Notes
+	Freeform
+	TV
+	Music
+	Keynote
+	Numbers
+	Pages
+	'System Settings'
+	'App Store'
+)
+
+for label in "${remove_labels[@]}"; do
+	dockutil --no-restart --remove "${label}" || true
+done
+
+# added in reverse order
+declare -a add_labels=(
+	'/Applications/VLC.app'
+	'/Applications/IntelliJ IDEA.app'
+	'/Applications/Visual Studio Code.app'
+	'/Applications/Discord.app'
+	'/Applications/Slack.app'
+	'/Applications/Google Chrome.app'
+	'/Applications/Firefox.app'
+	'/Applications/Ghostty.app'
+)
+
+for label in "${add_labels[@]}"; do
+	dockutil --no-restart --remove "${label}" || true
+	dockutil --add "${label}" --position beginning || true
+done
