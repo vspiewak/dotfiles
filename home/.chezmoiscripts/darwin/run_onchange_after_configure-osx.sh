dcf990f43c302f4acb3724dc50a7cbfa87be2aa1
Vincent Spiewak <vspiewak@gmail.com>
2025-07-16 21:09:56 +0200

    Cleanup

--- home/.chezmoiscripts/darwin/run_onchange_after_configure-osx.sh
+++ home/.chezmoiscripts/darwin/run_onchange_after_configure-osx.sh
@@ -2,14 +2,6 @@
 
 set -eufo pipefail
 
-trap 'killall Dock' EXIT
-
-# Set computer name (as done via System Preferences → Sharing)
-sudo scutil --set ComputerName "{{ .hostname }}"
-sudo scutil --set HostName "{{ .hostname }}"
-sudo scutil --set LocalHostName "{{ .hostname }}"
-sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "{{ .hostname }}"
-
 # Disable the sound effects on boot
 #sudo nvram SystemAudioVolume=" "
 
